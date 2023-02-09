e23f350de00b2422f23ba394ca2ca70f74ffe198
sun532818microbit <114642649+sun532818microbit@users.noreply.github.com>
2023-02-09 23:27:17 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    while (0 == 0) {
+        mbitbot.move_servo_pin(mbitbot.SePin.Sepin1, 300)
+    }
+})
+radio.sendNumber(0)
+basic.forever(function () {
+	
+})
